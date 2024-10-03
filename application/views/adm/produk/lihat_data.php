70264c1709ec5c636070068d1865395396be5bfa
agusbsi <38007043+agusbsi@users.noreply.github.com>
2024-10-03 11:02:09 +0700

    update sidebar mkt stok gudang

--- application/views/adm/produk/lihat_data.php
+++ application/views/adm/produk/lihat_data.php
@@ -39,7 +39,7 @@
               </div>
             </div>
             <hr>
-            <table id="example2" class="table table-bordered table-striped">
+            <table id="example1" class="table table-bordered table-striped">
               <thead>
                 <tr>
                   <th rowspan="2" style="width:3%">No</th>
